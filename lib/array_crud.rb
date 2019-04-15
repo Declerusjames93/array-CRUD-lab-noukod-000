5182b53d424b2b804bcbc1050f7144d6f3c68802
James Declerus <james.declerus@esih.edu>
2019-04-15 14:27:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-return array.index(index_number)
+return array[array.index(index_number)]
 end
 
 def retrieve_first_element_from_array(array)
